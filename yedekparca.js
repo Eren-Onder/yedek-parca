a03c1724f5acd7409be96e1056acaecf54a4f5ac
E2395-Eren <2395-Eren@users.noreply.github.com>
2021-01-10 19:33:30 +0100

    son hali

--- yedekparca.js
+++ yedekparca.js
@@ -1,13 +1,13 @@
 const parcaList = ["Egzos7t", "fAr", "RadYa32tö2r", "ayna ", " janT "];
 parcaList.forEach(function (element) {
   let yeniParca = element.trim().toUpperCase().split("");
-  let parcaIsmi = "KREMAG_";
+  let parcaIsmi = "KEREMAG_";
   for (let i = yeniParca.length - 1; i >= 0; i--) {
     if (isNaN(yeniParca[i])) {
       parcaIsmi += yeniParca[i];
     }
   }
   let gün = new Date().toDateString("en");
-  gün = gün.replaceAll(" ", "");
+  gün = gün.split(" ").join("");
   console.log(parcaIsmi + gün);
 });
